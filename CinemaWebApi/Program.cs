3c30bd8deb734b95f7c5187d9534539e75aed5f7
Hoang Quan <siluq.quan.1609@gmail.com>
2026-04-14 21:17:02 +0700

    update app

--- CinemaWebApi/Program.cs
+++ CinemaWebApi/Program.cs
@@ -26,11 +26,8 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseExceptionHandler();
 
